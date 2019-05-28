f04d7c6b808471a8ef9a2720028828ccb081efe8
Jeong Yu Han <jeong.yuhan@gmail.com>
2019-05-28 15:25:01 +0800

    Initial commit

---
+++ app/src/main/java/com/nus/alchemy/MainActivity.java
@@ -0,0 +1,89 @@
+ package com.nus.alchemy;
+
+ import android.app.ProgressDialog;
+ import android.content.Intent;
+ import android.os.Bundle;
+ import android.support.annotation.NonNull;
+ import android.support.v7.app.AppCompatActivity;
+ import android.text.TextUtils;
+ import android.view.View;
+ import android.widget.Button;
+ import android.widget.EditText;
+ import android.widget.TextView;
+ import android.widget.Toast;
+
+ import com.google.android.gms.tasks.OnCompleteListener;
+ import com.google.android.gms.tasks.Task;
+ import com.google.firebase.auth.AuthResult;
+ import com.google.firebase.auth.FirebaseAuth;
+
+//the login Activity
+
+ public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView registrationPageTextView;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        registrationPageTextView = (TextView) findViewById(R.id.registrationPageTextView);
+        emailEditText = (EditText) findViewById(R.id.emailEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        progressDialog = new ProgressDialog(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null) {
+            //user already logged in
+            //go to Profile if already registered properly, else go to the requirements page
+        }
+
+        registrationPageTextView.setOnClickListener(this);
+        loginButton.setOnClickListener(this);
+    }
+
+     @Override
+     public void onClick(View v) {
+         if (v == registrationPageTextView) {
+             Intent goToRegistrationPage = new Intent(this, RegistrationActivity.class);
+             finish();
+             startActivity(goToRegistrationPage);
+         }
+         if (v == loginButton) {
+             userLogin();
+         }
+     }
+
+     private void userLogin() {
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "incomplete user details", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("Wait for Login");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(
+                this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if (task.isSuccessful()) {
+
+                            //go to either the profile or the reg details page
+                        }
+                    }
+                }
+        );
+     }
+
+ }
---
+++ app/src/main/java/com/nus/alchemy/RegistrationActivity.java
@@ -0,0 +1,88 @@
+package com.nus.alchemy;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button registerButton;
+    private TextView loginPageTextView;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        emailEditText = (EditText) findViewById(R.id.emailEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        registerButton = (Button) findViewById(R.id.registerButton);
+        loginPageTextView = (TextView) findViewById(R.id.loginPageTextView);
+        progressDialog = new ProgressDialog(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        registerButton.setOnClickListener(this);
+        loginPageTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == registerButton) {
+            registerUser();
+        }
+        if (v == loginPageTextView) {
+            Intent goToLoginPage = new Intent(this, MainActivity.class);
+            startActivity(goToLoginPage);
+        }
+
+
+    }
+
+    private void registerUser() {
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Email and Password is required.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("Registering...");
+        progressDialog.show();
+
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password).
+                addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "successfull", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "fail", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        progressDialog.hide();
+
+    }
+
+}
