da0ad9e2ca92f1eb239d5e430c867f33e60ae206
Jeong Yu Han <jeong.yuhan@gmail.com>
2019-06-16 00:34:26 +0800

    chat.2

---
+++ app/src/main/java/com/nus/alchemy/Model/ChatObject.java
@@ -0,0 +1,14 @@
+package com.nus.alchemy.Model;
+
+public class ChatObject {
+
+    String chatID;
+    String title;
+
+    public ChatObject(String chatID) {
+        this.chatID = chatID;
+    }
+
+    public String getChatID() {return chatID; }
+
+}
