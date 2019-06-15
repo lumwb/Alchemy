4d5498c7eeb17f366c0b07344f559595c6ea6578
Jeong Yu Han <jeong.yuhan@gmail.com>
2019-06-15 14:15:47 +0800

    Commit nav bar

---
+++ app/src/main/java/com/nus/alchemy/MyChatsActivity.java
@@ -0,0 +1,50 @@
+package com.nus.alchemy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MyChatsActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_chats);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(3);
+        menuItem.setChecked(true);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.myProfile:
+                        Intent toMyProfile = new Intent(getApplicationContext(), ProfileActivity.class);
+                        startActivity(toMyProfile);
+                        finish();
+                        break;
+                    case R.id.myEvents:
+                        Intent toMyEvents = new Intent(getApplicationContext(), MyEventsActivity.class);
+                        startActivity(toMyEvents);
+                        finish();
+                        break;
+                    case R.id.todayEvents:
+                        Intent toTodaysEvents = new Intent(getApplicationContext(), TodaysEventsActivity.class);
+                        startActivity(toTodaysEvents);
+                        finish();
+                        break;
+                    case R.id.myChats:
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+}
