0b68130428e7a463efbba673e93cdb50d27f6d1f
Jeong Yu Han <jeong.yuhan@gmail.com>
2019-07-28 20:35:37 +0800

    COMMIT progress dialog to prevent crash

--- app/src/main/java/com/nus/alchemy/ForgotActivity.java
+++ app/src/main/java/com/nus/alchemy/ForgotActivity.java
@@ -2,23 +2,34 @@ package com.nus.alchemy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotActivity extends AppCompatActivity implements View.OnClickListener{
 
     TextView backToLoginTextView;
+    EditText enterEmailEditText;
+    Button resetPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot);
-
         backToLoginTextView = (TextView) findViewById(R.id.backToLoginTextView);
         backToLoginTextView.setOnClickListener(this);
+        enterEmailEditText = findViewById(R.id.EnterEmailEditText);
+        resetPassword= findViewById(R.id.resetButton);
+        resetPassword.setOnClickListener(this);
     }
 
     @Override
@@ -30,6 +41,29 @@ public class ForgotActivity extends AppCompatActivity implements View.OnClickLis
             startActivity(backToLogin);
             finish();
             return;
+        }
+        if (v == resetPassword) {
+            final String email = enterEmailEditText.getText().toString().trim();
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(getApplicationContext(), "Invalid input for email", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            FirebaseAuth.getInstance().sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getApplicationContext(), "An email has been sent to: " + email, Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                        return;
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), task.getException().toString(), Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }
+            });
         }
     }
 }
