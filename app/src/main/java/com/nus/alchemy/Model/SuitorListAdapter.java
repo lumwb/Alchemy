e51d40993fbc71123ecf4ae3edf4d326cf95ce58
weiboon <lumweiboon@gmail.com>
2019-07-28 23:48:25 +0800

    fix bug

--- app/src/main/java/com/nus/alchemy/Model/SuitorListAdapter.java
+++ app/src/main/java/com/nus/alchemy/Model/SuitorListAdapter.java
@@ -2,12 +2,12 @@ package com.nus.alchemy.Model;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -61,7 +61,7 @@ public class SuitorListAdapter extends RecyclerView.Adapter<SuitorListAdapter.Su
     public class SuitorListViewHolder extends RecyclerView.ViewHolder{
         public TextView mSuitor;
         public Button mButton;
-        public LinearLayout mSuitorLayout;
+        public ConstraintLayout mSuitorLayout;
         public SuitorListViewHolder(View view) {
             super(view);
             mSuitor = view.findViewById(R.id.suitorName);
