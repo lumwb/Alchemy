7c50b3a47104d1d331a18f7a2039ccf61911419a
Jeong Yu Han <jeong.yuhan@gmail.com>
2019-07-28 21:16:16 +0800

    COMMIT FIX DB LINK

--- app/src/main/java/com/nus/alchemy/GroupChatActivity.java
+++ app/src/main/java/com/nus/alchemy/GroupChatActivity.java
@@ -53,9 +53,6 @@ public class GroupChatActivity extends AppCompatActivity implements View.OnClick
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_chat);
         currentGroupName = getIntent().getExtras().get("currentGroupName").toString();
-
-        currentGroupName = "Jeong's Activity";
-
         groupHost = getIntent().getExtras().get("groupHost").toString();
         initAttributes();
         getUserInfo();
